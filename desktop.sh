97253145f0962fb49a7e243c335056edec5d76e2
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-04-28 00:35:36 +0300

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -1,11 +1,11 @@
 # Prompt the user for their username
 read -p "Enter your username: " username
 
-goverlay_desktop_file="/usr/share/applications/orrh.desktop"
+goverlay_desktop_file="/home/$USER/.local/share/applications/orrh.desktop"
 sudo echo "[Desktop Entry]" > "$goverlay_desktop_file"
 sudo echo "Name=OldRoblox" >> "$goverlay_desktop_file"
 sudo echo "Exec=wine /home/$USER/.wine/drive_c/ProgramData/OnlyRetroRobloxHere/OnlyRetroRobloxHere.exe" >> "$goverlay_desktop_file"
-sudo echo "Icon=/usr/share/icons/ORRH.png" >> "$goverlay_desktop_file"
+sudo echo "Icon=/home/$USER/.local/share/applications/ORRH.png" >> "$goverlay_desktop_file"
 sudo echo "Type=Application" >> "$goverlay_desktop_file"
 sudo echo "Categories=Games;" >> "$goverlay_desktop_file"
 
